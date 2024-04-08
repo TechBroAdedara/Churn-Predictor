a406b902afb234893b18b964d6ddd295d639e067
Adedara <courageadedara@gmail.com>
2024-04-08 21:01:05 +0100

    Changed preprocessing functions

--- preprocessing.py
+++ preprocessing.py
@@ -1,21 +1,55 @@
 import pandas as pd
-import pickle
+from sklearn.preprocessing import MinMaxScaler
+import joblib
 
-MinMaxScaler = pickle.load(open("pickle_files/MinMaxScaler.pkl","rb"))
+sc = joblib.load("pickle_files/scaler.save")
 
-def preprocess(data_df):
-    data_df = data_df.drop("customerID", axis=1)
+unique_categories = {
+    'InternetService': ['DSL', 'Fiber optic'],
+    'Contract' : ['Month-to-month','One year', 'Two year'], 
+    'PaymentMethod' : ['Mailed check', 'Electronic check', 'Bank transfer (automatic)',
+                       'Credit card (automatic)']
+}
 
+def preprocess_data(data_df, unique_categories=unique_categories):
+    # Drop unnecessary columns
+    #data_df = data_df.drop("customerID", axis=1)
+    
+    # Define binary mapping function
     def binaryMap(feature):
         return feature.map({"Yes": 1, "No":0})
 
-    data_df['gender'] = data_df['gender'].map({'Female':0, 'Male':1})
+    
+    binary_features_list = ['SeniorCitizen', 'Dependents','PhoneService', 'MultipleLines','OnlineSecurity', 'TechSupport', 'StreamingTV', 'StreamingMovies','PaperlessBilling']
+    data_df[binary_features_list] = data_df[binary_features_list].apply(binaryMap)
+    
+    # Create dummy variables
+    if unique_categories:
+        for column, categories in unique_categories.items():
+            data_df[column] = pd.Categorical(data_df[column], categories=categories)
+    data_df = pd.get_dummies(data_df, dtype=int)
+    
+    # Normalize numerical features
+    data_df[['tenure', 'MonthlyCharges', 'TotalCharges']] = sc.transform(data_df[['tenure', 'MonthlyCharges', 'TotalCharges']])
 
-    list_to_binarize = ['Churn', 'Dependents', 'PhoneService', 'PaperlessBilling', 'SeniorCitizen', 'Partner']
-    data_df[list_to_binarize] = data_df[list_to_binarize].apply(binaryMap)
-    data_df = pd.get_dummies(data_df, dtype=int, drop_first=True)
-
-    #Normalizing numerical features
-    data_df['tenure'] = MinMaxScaler.transform(data_df[['tenure']])
-    data_df['MonthlyCharges'] = MinMaxScaler.transform(data_df[['tenure']])
-    data_df['TotalCharges'] = MinMaxScaler.transform(data_df[['tenure']])
+    useful_columns = ['SeniorCitizen',
+                        'Dependents',
+                        'tenure',
+                        'PhoneService',
+                        'MultipleLines',
+                        'OnlineSecurity',
+                        'TechSupport',
+                        'StreamingTV',
+                        'StreamingMovies',
+                        'PaperlessBilling',
+                        'MonthlyCharges',
+                        'TotalCharges',
+                        'InternetService_DSL',
+                        'InternetService_Fiber optic',
+                        'Contract_Month-to-month',
+                        'Contract_Two year',
+                        'PaymentMethod_Electronic check']
+                    
+    data_df = data_df.loc[:,useful_columns]
+    
+    return data_df
